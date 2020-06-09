10770e6d1d4de5f5a71cf64c813fa4cf3c30e3d8
AbhijeetNandvikar <47082922+AbhijeetNandvikar@users.noreply.github.com>
2020-06-09 15:13:24 +0530

    backend created

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const db = require('./model/taskDocument.js')
+const app = express()
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send(db.getTasks());
+})
+
+app.post('/task', (req, res) => {
+    res.send(db.createTask(`hi`, `hi`, true))
+})
+
+app.delete('/:id', (req, res) => {
+    res.send(db.deleteTask(req.params.id))
+})
+
+port = process.env.port || 3000
+
+app.listen(port, console.log(`Listening on port ${port}`))
+
+
+// db.deleteTask(`5edde3fdb1efce35306f03f9`);
---
+++ model/taskDocument.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/to-do-app')
+    .then(console.log('connected to db'))
+    .catch(err => console.log(err))
+
+const taskSchema = mongoose.Schema({
+    title: String,
+    desc: String,
+    status: Boolean,
+    time: Date,
+})
+
+const taskDocument = mongoose.model('Tasks', taskSchema)
+
+// creating new tasks
+async function createTask(t, d, s) {
+    const newTask = new taskDocument({
+        title: t,
+        desc: d,
+        status: s,
+        time: Date.now(),
+    })
+    return await newTask.save()
+
+}
+
+// createTask()
+
+
+// accessing the document
+
+async function getTasks() {
+    return await taskDocument.find({})
+}
+
+
+// Updating the documents 
+
+async function updateTask(id, t, d, s) {
+    return await taskDocument.update(
+        {
+            _id: `${id}`
+        },
+        {
+            title: t,
+            desc: d,
+            status: s,
+            time: Date.now(),
+        })
+
+}
+
+// updateTask(`5ed09d6b5e538e23b05b1252`)
+
+async function deleteTask(id) {
+    return await taskDocument.deleteOne({
+        _id: `${id}`
+    })
+
+}
+
+// deleteTask(`5ed09d6b5e538e23b05b1252`);
+
+// Exporting taskDocument
+
+module.exports = { taskDocument, getTasks, createTask, updateTask, deleteTask };
